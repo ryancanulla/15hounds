465a3270c9dea7a9d95227ef200510974a1cae1e
Ryan Canulla <ryancanulla@gmail.com>
2017-06-13 19:44:23 -0400

    remove the old project

--- app/main.ts
+++
@@ -1,6 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import {GOOGLE_MAPS_PROVIDERS} from 'angular2-google-maps/core';
-
-import { AppModule } from './app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
--- app/service/service.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'service',
-    styleUrls: [ 'app/service/service.component.css' ],
-    templateUrl: 'app/service/service.component.html'
-})
-export class ServiceComponent { }
